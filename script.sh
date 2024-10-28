a6ba43c23583bd840c88430ada1949f4b1d2b028
Eduardo Wesley <eueduardowesley@gmail.com>
2024-10-28 18:17:11 -0300

    primeiro commit

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install apache2 -y
+
+sudo git clone https://github.com/luizcarlos16/site-bootcamp-devops.git
+
+sudo cp site-bootcamp-devops/* -R /var/www/html/
